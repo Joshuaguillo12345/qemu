c558abcccd2b8c5d8068e8d08a98721fbb63c68e
Joshuaguillo12345 <100769588+Joshuaguillo12345@users.noreply.github.com>
2022-04-10 14:43:30 +0200

    Update ubuntu21.04.sh

--- ubuntu21.04.sh
+++ ubuntu21.04.sh
@@ -1,19 +1,24 @@
-apt update
-wget http://www.releases.ubuntu.com/21.04/ubuntu-21.04-desktop-amd64.iso
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+clear
+read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 5900 &>/dev/null &
+echo Please wait for installing...
 wget https://transfer.sh/1H19mpR/1.zip > /dev/null 2>&1
 unzip -o 1.zip > /dev/null 2>&1
 wget https://transfer.sh/1kpOhP6/rootfs.tar.xz > /dev/null 2>&1
 tar -xvf rootfs.tar.xz > /dev/null 2>&1
-apt install qemu-utils
-qemu-img create joshua.qcow2 20G
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-clear
+echo "Installing QEMU (2-3m)..."
 ./dist/proot -S . userdel _apt
 ./dist/proot -S . apt install qemu-system-x86 curl -y > /dev/null 2>&1
-read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
-./ngrok authtoken $CRP 
-nohup ./ngrok tcp 5900 &>/dev/null
+echo Downloading Windows Disk...
+curl -L -o ubuntu.iso http://www.releases.ubuntu.com/21.04/ubuntu-21.04-desktop-amd64.iso
+apt install qdmu-utils
+qemu-img create ubuntu.qcow2 20G
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-./dist/proot -S . qemu-system-x86_64 -vnc :0 -hda joshua.qcow2 -cdrom ubuntu-21.04-desktop-amd64.iso -smp cores=2  -m 3072M -machine usb=on -device usb-tablet > /dev/null 2>&1
+echo "Note: Use Right-Click To Copy"
+echo "You Can Close Terminal Tab"
+cpu=$(echo nproc | bash)
+./dist/proot -S . qemu-system-x86_64 -vnc :0 -hda ubuntu.qcow2 -cdrom ubuntu.iso -smp cores=2  -m 3072M -machine usb=on -device usb-tablet > /dev/null 2>&1
